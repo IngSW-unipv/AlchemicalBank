cb1cfdf2c130bdb4bc58373d3db839b0000d93c7
Riccardo <riccardo.doveri01@unipv.it>
2019-03-12 20:32:47 +0100

    mago semplice

--- src/it/unipv/ingsw/alchemicalbank/wizards/DoveriRiccardoWizard.java
+++ src/it/unipv/ingsw/alchemicalbank/wizards/DoveriRiccardoWizard.java
@@ -4,21 +4,41 @@ import it.unipv.ingsw.alchemicalbank.Decision;
 import it.unipv.ingsw.alchemicalbank.Wizard;
 // Doveri Riccardo 448794
 public class DoveriRiccardoWizard extends Wizard {
+
+    private int foundValue;
+    private int timespan;
+
     private int year;
     private int order;
     private long yourCoins;
     private long partnerCoins;
-    private  int time;
+
+    private int time;
     private int yourRevenue;
     private int partnerRevenue;
-    private int soglia1 = 11;
-    private int soglia2 = 10;
+
+    private int punteggio = 0;
+    private int soglia = 10;
+
+    @Override
+    public Decision askKeepOrLiquidate(int fundValue, int timespan) {
+        this.foundValue = fundValue;
+        this.timespan = timespan;
+
+        if (timespan >= soglia) {
+            return Decision.LIQUIDATE_FUND;
+        }
+        else return Decision.KEEP_FUND;
+
+    }
+
     @Override
     public void newFund(int year, int order, long yourCoins, long partnerCoins) {
         this.year = year;
         this.order = order;
         this.yourCoins = yourCoins;
         this.partnerCoins = partnerCoins;
+
 
     }
 
@@ -27,61 +47,12 @@ public class DoveriRiccardoWizard extends Wizard {
         this.time = time;
         this.yourRevenue = yourRevenue;
         this.partnerRevenue = partnerRevenue;
-        impostaSogliaFine();
-    }
-    private void alzaSoglia() {
-        if (order == 1) {
-            if (soglia1 <= 9) {
-                soglia1 = soglia1 +2;
-            }
-        }
 
+        if ((yourRevenue-yourCoins) > (partnerRevenue-partnerCoins)) {
+            punteggio++;
+        }
         else {
-            if (soglia2 <= 10) {
-                soglia2 = soglia2 +2;
-            }
+            punteggio--;
         }
-    }
-    private void abbassaSoglia() {
-        if (order == 1) {
-            if (soglia1 >= 3) {
-                soglia1 = soglia1 -2;
-            }
-        }
-
-        else {
-            if (soglia2 >= 4) {
-                soglia2 = soglia2 -2;
-            }
-        }
-    }
-
-    private void impostaSoglia(){
-        if (yourCoins >= partnerCoins) {
-            abbassaSoglia();
-        }
-        else alzaSoglia();
-    }
-    private void impostaSogliaFine() {
-        if ((yourRevenue-yourCoins)>(partnerRevenue-partnerCoins)) {
-        }
-        else abbassaSoglia();
-    }
-
-    @Override
-    public Decision askKeepOrLiquidate(int fundValue, int timespan) {
-        impostaSoglia();
-       if (order == 1) {
-           if (timespan >= soglia1) {
-               return Decision.LIQUIDATE_FUND;
-           }
-           else return Decision.KEEP_FUND;
-       }
-       else {
-           if (timespan >= soglia2) {
-               return Decision.LIQUIDATE_FUND;
-           }
-           else return Decision.KEEP_FUND;
-       }
     }
 }
