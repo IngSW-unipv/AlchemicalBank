0b0a45cbde39afd94d6c834f51e297daa30537c9
cresc <riccardo.crescenti01@universitadipavia.it>
2019-03-12 23:33:38 +0100

    Mago Crescenti

--- src/it/unipv/ingsw/alchemicalbank/wizards/CrescentiRiccardo.java
+++ src/it/unipv/ingsw/alchemicalbank/wizards/CrescentiRiccardo.java
@@ -2,13 +2,19 @@ package it.unipv.ingsw.alchemicalbank.wizards;
 
 import it.unipv.ingsw.alchemicalbank.Decision;
 import it.unipv.ingsw.alchemicalbank.Wizard;
-//crescenti riccardo 447250
 public class CrescentiRiccardo extends Wizard {
+
+    //crescenti riccardo 447250
 
     @Override
     public Decision askKeepOrLiquidate(int fundValue, int timespan) {
-        return Decision.KEEP_FUND;
+
+
+        if (timespan > 9 && timespan < 12) {
+            return Decision.LIQUIDATE_FUND;
+        } else {
+            return Decision.KEEP_FUND;
+        }
+
     }
-
-
 }
