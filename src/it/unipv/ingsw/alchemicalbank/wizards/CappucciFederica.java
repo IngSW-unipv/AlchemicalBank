beed341ba4fdb4b4c7b8a9c8542048c75327abb7
FedericaCappucci <luciafederica.cappucci01@universitadipavia.it>
2019-03-12 16:32:59 +0000

    inizializza classe mago

--- src/it/unipv/ingsw/alchemicalbank/wizards/CappucciFederica.java
+++ src/it/unipv/ingsw/alchemicalbank/wizards/CappucciFederica.java
@@ -1,4 +1,24 @@
 package it.unipv.ingsw.alchemicalbank.wizards;
+//Cappucci Lucia Federica 446400
 
-public class CappucciFederica {
+import it.unipv.ingsw.alchemicalbank.Decision;
+import it.unipv.ingsw.alchemicalbank.Wizard;
+
+public class CappucciFederica extends Wizard {
+    private int order=0;
+    private long yourCoins=100000;
+
+    @Override
+    public Decision askKeepOrLiquidate(int fundValue, int timespan) {
+        if(timespan==7)
+        {
+            return Decision.LIQUIDATE_FUND;
+        }
+        return Decision.KEEP_FUND;
+    }
+    public void newFund(int year, int order, long yourCoins, long partnerCoins) {
+        this.order=order;
+        this.yourCoins=yourCoins+this.yourCoins;
+    }
+
 }
