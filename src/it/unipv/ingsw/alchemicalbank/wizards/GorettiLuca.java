b23a005ab2ce49df0dff05edb657c9604f715adc
Adriano <adriano.cotta.ramusino@gmail.com>
2019-03-12 21:31:41 +0100

    Strategia birbante

--- src/it/unipv/ingsw/alchemicalbank/wizards/GorettiLuca.java
+++ src/it/unipv/ingsw/alchemicalbank/wizards/GorettiLuca.java
@@ -3,10 +3,11 @@ package it.unipv.ingsw.alchemicalbank.wizards;
 import it.unipv.ingsw.alchemicalbank.Decision;
 import it.unipv.ingsw.alchemicalbank.Wizard;
 
-public class SecchiAndrea_GorettiLuca extends Wizard {
-    //Secchi Andrea, Goretti Luca
-    public Decision askKeepOrLiquidate(int fundValue, int timespan) {
+public class GorettiLuca extends Wizard {
+    //Luca Goretti
 
+    @Override
+    public Decision askKeepOrLiquidate(int fundValue, int timespan) {
         if (timespan%2==0) {
             if (timespan>=10)
                 return Decision.LIQUIDATE_FUND;
@@ -20,8 +21,4 @@ public class SecchiAndrea_GorettiLuca extends Wizard {
                 return Decision.KEEP_FUND;
         }
     }
-
-
-        }
-
-
+}
