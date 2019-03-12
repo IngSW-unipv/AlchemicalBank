d17f553267d4683cd90db94547932077c80e088a
lorenzo0206 <lorenzo.mariani01@universitadipavia.it>
2019-03-12 16:51:29 +0100

    Modified syntax for if statement

--- src/it/unipv/ingsw/alchemicalbank/wizards/MarazziFedericoMarianiLorenzo.java
+++ src/it/unipv/ingsw/alchemicalbank/wizards/MarazziFedericoMarianiLorenzo.java
@@ -3,18 +3,16 @@ package it.unipv.ingsw.alchemicalbank.wizards;
 import it.unipv.ingsw.alchemicalbank.Decision;
 import it.unipv.ingsw.alchemicalbank.Wizard;
 
-public class MarazziFedericoMarianiLorenzo extends Wizard{
+public class MarazziFedericoMarianiLorenzo extends Wizard {
 
     //MARAZZI FEDERICO 448929
     //MARIANI LORENZO 448295
 
-    private final int MAX_MAGO1 = 11, MAX_MAGO2 = 12;
-
     @Override
-    public Decision askKeepOrLiquidate(int fundValue, int timespan){
-        if(timespan == MAX_MAGO1 || timespan == MAX_MAGO2){
+    public Decision askKeepOrLiquidate(int fundValue, int timespan) {
+        if (timespan >= 11) {
             return Decision.LIQUIDATE_FUND;
-        } else {
+        } else{
             return Decision.KEEP_FUND;
         }
     }
