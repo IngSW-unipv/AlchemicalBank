1a5aab200482eff309b120d1a6755ec5e06006b4
SaraMacri <sara.macri02@universitadipavia.it>
2019-03-12 19:47:15 +0100

    primo tentativo

---
+++ src/it/unipv/ingsw/alchemicalbank/wizards/MacriSara.java
@@ -0,0 +1,16 @@
+package it.unipv.ingsw.alchemicalbank.wizards;
+
+import it.unipv.ingsw.alchemicalbank.Decision;
+import it.unipv.ingsw.alchemicalbank.Wizard;
+//Sara Macri 446087
+public class MacriSara extends Wizard {
+    @Override
+    public Decision askKeepOrLiquidate(int fundValue, int timespan) {
+        if (timespan < 10) {
+            return Decision.KEEP_FUND;
+        }
+        else{
+            return Decision.LIQUIDATE_FUND;
+        }
+    }
+}
