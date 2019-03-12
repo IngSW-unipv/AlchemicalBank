331530bde59f2719eea082512c54172026d7223f
FedericaCappucci <luciafederica.cappucci01@universitadipavia.it>
2019-03-12 22:32:42 +0000

    Broncano Estefania

---
+++ src/it/unipv/ingsw/alchemicalbank/wizards/BroncanoEstefania.java
@@ -0,0 +1,11 @@
+package it.unipv.ingsw.alchemicalbank.wizards;
+
+import it.unipv.ingsw.alchemicalbank.Decision;
+import it.unipv.ingsw.alchemicalbank.Wizard;
+
+public class BroncanoEstefania extends Wizard {
+    @Override
+    public Decision askKeepOrLiquidate(int fundValue, int timespan) {
+        return null;
+    }
+}
