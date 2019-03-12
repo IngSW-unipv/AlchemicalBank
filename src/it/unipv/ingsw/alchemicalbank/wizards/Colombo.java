f6c574328432bd1e396c8f2167f00393f9ce5cfd
luca <luca.colombo04@universitadipavia.it>
2019-03-12 23:59:34 +0100

    Colombo wizard

---
+++ src/it/unipv/ingsw/alchemicalbank/wizards/Colombo.java
@@ -0,0 +1,32 @@
+package it.unipv.ingsw.alchemicalbank.wizards;
+
+import it.unipv.ingsw.alchemicalbank.Decision;
+import it.unipv.ingsw.alchemicalbank.Wizard;
+
+public class Colombo extends Wizard {
+
+    private int soglia = 11;
+
+    @Override
+    public Decision askKeepOrLiquidate(int fundValue, int timespan) {
+        this.foundValue = fundValue;
+        this.timespan = timespan;
+
+        if (year > 45) {
+            if (partnerCoins>yourCoins) {
+                soglia = 9;
+            }
+            else {
+                soglia = 10;
+            }
+        }
+        if (timespan >= soglia) {
+            return Decision.LIQUIDATE_FUND;
+        }
+        else return Decision.KEEP_FUND;
+
+    }
+
+
+    }
+}
