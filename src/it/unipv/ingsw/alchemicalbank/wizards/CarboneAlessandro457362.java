8d8920191d0bd252c4cbabe3df30c68c73f24a2a
Alessandro Carbone <alessandro.carbone02@universitadipavia.it>
2020-03-17 15:26:00 +0100

    Cambiato < in <=

--- src/it/unipv/ingsw/alchemicalbank/wizards/CarboneAlessandro457362.java
+++ src/it/unipv/ingsw/alchemicalbank/wizards/CarboneAlessandro457362.java
@@ -12,7 +12,7 @@ public class CarboneAlessandro457362 extends Wizard {
 	@Override
 	public Decision askKeepOrLiquidate(int fundValue, int timespan) {
 		// TODO Auto-generated method stub
-		if(fundValue<500)
+		if(fundValue<=500)
 			return Decision.KEEP_FUND;
 		else
 			return Decision.LIQUIDATE_FUND;
