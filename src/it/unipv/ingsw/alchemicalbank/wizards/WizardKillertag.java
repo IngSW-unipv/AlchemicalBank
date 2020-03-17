c38152978dffb53c45cd738fb5d8f325873354cc
killertag <killertag@gmail.com>
2020-03-17 22:32:44 +0100

    aggiunto wizard killertag

--- src/it/unipv/ingsw/alchemicalbank/wizards/WizardKillertag.java
+++ src/it/unipv/ingsw/alchemicalbank/wizards/WizardKillertag.java
@@ -9,7 +9,7 @@ public class WizardKillertag extends Wizard {
 	public Decision askKeepOrLiquidate(int fundValue, int timespan) {
 		if(fundValue<90000) {
 			return Decision.KEEP_FUND;
-			//aggiungo un commento 
+
 		}else {
 			return Decision.LIQUIDATE_FUND;
 		}
