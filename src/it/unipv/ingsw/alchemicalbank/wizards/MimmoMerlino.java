bbba05910dd0e8e13ad6b15e42f8c54c847a0351
Domenico Ragusa <domenico.ragusa01@universitadipavia.it>
2020-03-17 15:29:40 +0100

    Aggiunta MimmoMerlino

---
+++ src/it/unipv/ingsw/alchemicalbank/wizards/MimmoMerlino.java
@@ -0,0 +1,15 @@
+package it.unipv.ingsw.alchemicalbank.wizards;
+
+import it.unipv.ingsw.alchemicalbank.Decision;
+import it.unipv.ingsw.alchemicalbank.Wizard;
+
+//Domenico Ragusa 454220
+
+public class MimmoMerlino extends Wizard {
+
+	@Override
+	public Decision askKeepOrLiquidate(int fundValue, int timespan) {
+			return Decision.KEEP_FUND;
+	}
+
+}
