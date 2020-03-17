5b8a9b9caf6f717cc2a7b08be200dae24fefcc7a
killer <killer@DESKTOP-M0N20U8>
2020-03-17 22:10:00 +0100

    nuovo mago killertag

---
+++ src/it/unipv/ingsw/alchemicalbank/wizards/WizardKillertag.java
@@ -0,0 +1,17 @@
+package it.unipv.ingsw.alchemicalbank.wizards;
+
+import it.unipv.ingsw.alchemicalbank.Decision;
+import it.unipv.ingsw.alchemicalbank.Wizard;
+
+public class WizardKillertag extends Wizard {
+
+	@Override
+	public Decision askKeepOrLiquidate(int fundValue, int timespan) {
+		if(fundValue<90000) {
+			return Decision.KEEP_FUND;
+		}else {
+			return Decision.LIQUIDATE_FUND;
+		}
+	}
+
+}
