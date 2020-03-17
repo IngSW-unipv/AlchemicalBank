7d49a4cd645f6e01539851ab315ad68fca5b1df0
davide <davide.spalenza01@universitadipavia.it>
2020-03-17 19:45:03 +0100

    Modifica logica

--- src/it/unipv/ingsw/alchemicalbank/wizards/DavideSpalenza.java
+++ src/it/unipv/ingsw/alchemicalbank/wizards/DavideSpalenza.java
@@ -4,11 +4,12 @@ import it.unipv.ingsw.alchemicalbank.Decision;
 import it.unipv.ingsw.alchemicalbank.Wizard;
 
 //Davide Spalenza 454097
+
 public class DavideSpalenza extends Wizard {
 
 	@Override
 	public Decision askKeepOrLiquidate(int fundValue, int timespan) {
-		if(fundValue<18000) return Decision.KEEP_FUND;
+		if(fundValue<=18500) return Decision.KEEP_FUND;
 		else return Decision.LIQUIDATE_FUND;
 	}
 
