353f0a37f1df578c238059e1d4f0e692b916e009
simon <simone.minerva@gmail.com>
2020-03-18 18:15:30 +0100

    modificato mago

--- src/it/unipv/ingsw/alchemicalbank/wizards/MinervaWizard.java
+++ src/it/unipv/ingsw/alchemicalbank/wizards/MinervaWizard.java
@@ -6,21 +6,10 @@ import it.unipv.ingsw.alchemicalbank.Wizard;
 import java.util.Random;
 
 public class MinervaWizard extends Wizard {
-
-    private Random randomGen = new Random();
-    private long partnerCoins = 0;
-    private int year;
-
-    @Override
-    public void newFund(int year, int order, long yourCoins, long partnerCoins) {
-        this.partnerCoins = partnerCoins;
-        this.year = year;
-    }
-
     @Override
     public Decision askKeepOrLiquidate(int fundValue, int timespan) {
 
-        if(timespan==8||timespan==9) return Decision.LIQUIDATE_FUND;
+        if(timespan==10||timespan==11) return Decision.LIQUIDATE_FUND;
         else return Decision.KEEP_FUND;
 
     }
