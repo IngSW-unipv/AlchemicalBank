6f07e7d8a305585272e41403aa938f3df16c701b
AlessandroCapici <alessandro.capici01@universitadipavia.it>
2020-03-18 16:04:20 +0100

    migliorato

--- src/it/unipv/ingsw/alchemicalbank/wizards/LooserWizard.java
+++ src/it/unipv/ingsw/alchemicalbank/wizards/LooserWizard.java
@@ -4,11 +4,31 @@ import it.unipv.ingsw.alchemicalbank.Decision;
 import it.unipv.ingsw.alchemicalbank.Wizard;
 
 public class LooserWizard extends Wizard {
+	private long mcoin;
+	private long ycoin;
+	private int start;
 
 	@Override
 	public Decision askKeepOrLiquidate(int fundValue, int timespan) {
-		// TODO Auto-generated method stub
-		return Decision.KEEP_FUND;
+		if (start == 1) {
+			if (fundValue>15000) {
+				return Decision.LIQUIDATE_FUND;
+			}
+			return Decision.KEEP_FUND;
+			
+		} else {
+			if (timespan ==12) {
+				return Decision.LIQUIDATE_FUND;
+			}
+			return Decision.KEEP_FUND;
+		}
+
+	}
+
+	public void newFund(int year, int order, long yourCoins, long partnerCoins) {
+		start = order;
+		mcoin = yourCoins;
+		ycoin = partnerCoins;
 	}
 
 }
