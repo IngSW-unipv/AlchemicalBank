3afc876b4cf56c0c4fe83dc71c4eb810652d6fac
AlessandroCapici <alessandro.capici01@universitadipavia.it>
2020-03-18 16:54:17 +0100

    cambiato strategia

--- src/it/unipv/ingsw/alchemicalbank/wizards/LoserWizard.java
+++ src/it/unipv/ingsw/alchemicalbank/wizards/LoserWizard.java
@@ -4,31 +4,13 @@ import it.unipv.ingsw.alchemicalbank.Decision;
 import it.unipv.ingsw.alchemicalbank.Wizard;
 
 public class LoserWizard extends Wizard {
-	private long mcoin;
-	private long ycoin;
-	private int start;
-
 	@Override
 	public Decision askKeepOrLiquidate(int fundValue, int timespan) {
-		if (start == 1) {
-			if (fundValue>15000) {
-				return Decision.LIQUIDATE_FUND;
-			}
+		if (fundValue <= 18500)
 			return Decision.KEEP_FUND;
-			
-		} else {
-			if (timespan ==10) {
-				return Decision.LIQUIDATE_FUND;
-			}
-			return Decision.KEEP_FUND;
-		}
+		else
+			return Decision.LIQUIDATE_FUND;
 
-	}
-
-	public void newFund(int year, int order, long yourCoins, long partnerCoins) {
-		start = order;
-		mcoin = yourCoins;
-		ycoin = partnerCoins;
 	}
 
 }
