68a04089c48113c867d77f68d25c6cafd9139528
Frenkli-Buzhiqi <frenkli.buzhiqi01@universitadipavia.it>
2020-03-18 14:05:36 +0100

    Creazione FrenkliBuzhiqi

---
+++ src/it/unipv/ingsw/alchemicalbank/wizards/FrenkliBuzhiqi.java
@@ -0,0 +1,16 @@
+package it.unipv.ingsw.alchemicalbank.wizards;
+
+import it.unipv.ingsw.alchemicalbank.Decision;
+import it.unipv.ingsw.alchemicalbank.Wizard;
+
+// Frenkli Buzhiqi
+
+public class FrenkliBuzhiqi extends Wizard{
+
+	@Override
+	public Decision askKeepOrLiquidate(int fundValue, int timespan) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
