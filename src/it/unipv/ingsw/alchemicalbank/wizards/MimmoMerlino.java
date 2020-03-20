f7e54700234d1740b1e7870eed341850efcd10ab
Domenico Ragusa <domenico.ragusa01@universitadipavia.it>
2020-03-20 17:19:14 +0100

    pulito il codice

--- src/it/unipv/ingsw/alchemicalbank/wizards/MimmoMerlino.java
+++ src/it/unipv/ingsw/alchemicalbank/wizards/MimmoMerlino.java
@@ -3,64 +3,47 @@ package it.unipv.ingsw.alchemicalbank.wizards;
 import it.unipv.ingsw.alchemicalbank.Decision;
 import it.unipv.ingsw.alchemicalbank.Wizard;
 
-//Domenico Ragusa 454220
+//Domenico Ragusa 454220 🧙🏻‍♂
 
 public class MimmoMerlino extends Wizard {
 	private int mioOrdine;
-	private boolean chiudeBasso=false;
-	int differenza=0;
+	private boolean chiudeBasso=false, differenza;
 	
 	@Override
     public void newFund(int year, int order, long yourCoins, long partnerCoins) {
-		mioOrdine=order;
+		mioOrdine=order; //ottengo il mio ordine di scelta
 		
-		if(yourCoins > 2.5*partnerCoins)
-			differenza=1;
-		else
-			differenza=0;
+		if(yourCoins > 2.5*partnerCoins) //cerco di capire se posso rischiare guardando la differenza tra i guadagni del mio mago e quelli dell'avversario
+			differenza=true; 
+		else 
+			differenza=false;
 		
-		if(partnerCoins <= (year-1)*800)
+		if(partnerCoins <= (year-1)*800) //cerco di capire se l'avversario è uno che tende a chiudere presto il conto
 			chiudeBasso=true;
 	}
-	
-	@Override
-	 public void fundClosed(int time, int yourRevenue, int partnerRevenue) {
-    }
 
 	@Override
 	public Decision askKeepOrLiquidate(int fundValue, int timespan) {
-		if(fundValue > 16383)
+		if(fundValue > 16383) //se si arriva oltre l'11o mese chiudo il prima possibile
 			return Decision.LIQUIDATE_FUND;
 		
-		if(chiudeBasso) {
-			if(differenza==1) {
-				if(mioOrdine==1 && timespan==7)
+		if(chiudeBasso) { //se l'avversario è un tipo che tende a chiudere subito...
+			if(differenza==true) { //se c'è molta differenza provo a rischiare chiudendo dopo la metà dell'anno
+				if((mioOrdine==1 && timespan==7) | (mioOrdine==2 && timespan==8))
 					return Decision.LIQUIDATE_FUND;
-				else if(mioOrdine==2 && timespan==8)
+			}else{ //altrimenti provo a chiudere prima, sempre dopo la metà dell'anno (prima i guadagni sono molto bassi)
+				if((mioOrdine==1 && timespan==6) | (mioOrdine==2 && timespan==7))
 					return Decision.LIQUIDATE_FUND;
-			}else if(differenza==0) {
-				if(mioOrdine==1 && timespan==6)
-					return Decision.LIQUIDATE_FUND;
-				else if(mioOrdine==2 && timespan==7)
-					return Decision.LIQUIDATE_FUND;
-			}else {
-				return Decision.KEEP_FUND;
 			}
-		}else if(!chiudeBasso) {
-			if(differenza==1) {
-				if(mioOrdine==1 && timespan==9)
+		}else{ //se non è un tipo che chiude subito...
+			if(differenza==true) { //se c'è molta differenza cerco di avvicinarmi ai mesi più alti
+				if((mioOrdine==1 && timespan==9) | (mioOrdine==2 && timespan==10))
 					return Decision.LIQUIDATE_FUND;
-				else if(mioOrdine==2 && timespan==10)
+			}else{ //altrimenti cerco di anticiparlo
+				if((mioOrdine==1 && timespan==7) | (mioOrdine==2 && timespan==8))
 					return Decision.LIQUIDATE_FUND;
-			}else if(differenza==0){
-				if(mioOrdine==1 && timespan==7)
-					return Decision.LIQUIDATE_FUND;
-				else if(mioOrdine==2 && timespan==8)
-					return Decision.LIQUIDATE_FUND;
-			}else
-					return Decision.KEEP_FUND;
-			}else
-				return Decision.KEEP_FUND;
-		return Decision.KEEP_FUND;
+			}
+		}
+		return Decision.KEEP_FUND; //decisione di default se non si verificano le condizioni sopra
 	}
 }
