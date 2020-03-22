af8cc03cc7edc2dda2cf3c4ee43aba6b9dd3ab3d
irene schimperna <irene.schimperna@gmail.com>
2020-03-22 12:24:26 +0100

    Modificata strategia

--- src/it/unipv/ingsw/alchemicalbank/wizards/SchimpernaIrene.java
+++ src/it/unipv/ingsw/alchemicalbank/wizards/SchimpernaIrene.java
@@ -6,14 +6,60 @@ import it.unipv.ingsw.alchemicalbank.Wizard;
 // Schimperna Irene matr. 458248
 
 public class SchimpernaIrene extends Wizard {
-
+	private int order, year;
+	private long yourCoins, partnerCoins;
+	private boolean wonLastYear;
+	private int timeToClose;
+	
+	public SchimpernaIrene() {
+		super();
+		
+		wonLastYear = true;
+	}
+	
 	@Override
 	public Decision askKeepOrLiquidate(int fundValue, int timespan) {
-		if(timespan > 7)
+		if(timespan >= timeToClose) {
 			return Decision.LIQUIDATE_FUND;
-		else
+		} else {
 			return Decision.KEEP_FUND;
+		}
 		
+	}
+	
+	@Override
+	public void newFund(int year, int order, long yourCoins, long partnerCoins) {
+        this.year = year;
+		this.order = order;
+        this.yourCoins = yourCoins;
+        this.partnerCoins = partnerCoins;
+        timeToClose = timeToClose();
+    }
+	
+	@Override
+	public void fundClosed(int time, int yourRevenue, int partnerRevenue) {
+        if(yourRevenue < partnerRevenue) {
+        	wonLastYear = false;
+        } else {
+        	wonLastYear = true;
+        }
+    }
+	
+	private int timeToClose() {
+		int time = 11;
+		if(order == 2) {
+			time = 12;
+			if(!wonLastYear) {
+				time = time - 2;
+			}
+		}
+		if(year>=3 && yourCoins > 4*partnerCoins) {
+			if(order == 2)
+				time = 6;
+			else
+				time = 7;
+		}
+		return time;
 	}
 
 }
