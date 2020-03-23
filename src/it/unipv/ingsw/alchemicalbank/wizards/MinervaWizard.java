43bef7a7957b8957b05e7e09737e0d0cf9888e6e
simoneminervaunipv <simoneantonio.minerva01@universitadipavia.it>
2020-03-23 02:44:00 +0100

    modifica mago

--- src/it/unipv/ingsw/alchemicalbank/wizards/MinervaWizard.java
+++ src/it/unipv/ingsw/alchemicalbank/wizards/MinervaWizard.java
@@ -3,15 +3,17 @@ package it.unipv.ingsw.alchemicalbank.wizards;
 import it.unipv.ingsw.alchemicalbank.Decision;
 import it.unipv.ingsw.alchemicalbank.Wizard;
 
-import java.util.Random;
 
 public class MinervaWizard extends Wizard {
+
+    //Simone Antonio Minerva 444563
+
     @Override
     public Decision askKeepOrLiquidate(int fundValue, int timespan) {
 
-        if(timespan==11||timespan==10) return Decision.LIQUIDATE_FUND;
+        if(timespan==11||timespan==12) return Decision.LIQUIDATE_FUND;
         else return Decision.KEEP_FUND;
-        //test prova
+
 
     }
 
