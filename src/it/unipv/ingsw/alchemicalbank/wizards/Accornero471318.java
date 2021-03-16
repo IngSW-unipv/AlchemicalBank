d3c76d55edb40e437059ca0da7d4c1a8aabc6dc4
steveacco <stefano.accornero02@universitadipavia.it>
2021-03-16 18:31:58 +0100

    Aggiunto Accornero471318

---
+++ src/it/unipv/ingsw/alchemicalbank/wizards/Accornero471318.java
@@ -0,0 +1,18 @@
+package it.unipv.ingsw.alchemicalbank.wizards;
+
+//ACCORNERO STEFANO 471318
+
+import it.unipv.ingsw.alchemicalbank.Decision;
+import it.unipv.ingsw.alchemicalbank.Wizard;
+
+public class Accornero471318 extends Wizard {
+
+	@Override
+	public Decision askKeepOrLiquidate(int fundValue, int timespan) {
+		if (fundValue < 4000)
+			return Decision.KEEP_FUND;
+		else 
+			return Decision.LIQUIDATE_FUND;
+	}
+
+}
