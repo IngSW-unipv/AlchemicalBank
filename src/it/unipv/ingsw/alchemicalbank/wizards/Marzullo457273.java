f5d76f9b2f724de3df6f2ac5cb7d36cc57e5edb9
francesco-mrz <francesco.marzullo01@universitadipavia.it>
2021-03-16 16:29:34 +0100

    Aggiunto wizard Marzullo457273

---
+++ src/it/unipv/ingsw/alchemicalbank/wizards/Marzullo457273.java
@@ -0,0 +1,22 @@
+package it.unipv.ingsw.alchemicalbank.wizards;
+
+import com.sun.org.apache.xpath.internal.FoundIndex;
+
+import it.unipv.ingsw.alchemicalbank.Decision;
+import it.unipv.ingsw.alchemicalbank.Wizard;
+
+// Francesco Marzullo
+
+public class Marzullo457273 extends Wizard {
+
+	@Override
+	public Decision askKeepOrLiquidate(int fundValue, int timespan) {
+		// TODO Auto-generated method stub
+		if (fundValue > 130) {
+			return Decision.LIQUIDATE_FUND;
+		} else {
+			return Decision.KEEP_FUND;
+		}
+	}
+
+}
