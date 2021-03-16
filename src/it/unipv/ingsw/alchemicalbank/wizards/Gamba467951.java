4572914c48686f9287d07175f9bff9f253d1af10
mauriziogamba <mauriziogamba@localhost>
2021-03-16 16:08:52 +0100

    modifica

--- src/it/unipv/ingsw/alchemicalbank/wizards/Gamba467951.java
+++ src/it/unipv/ingsw/alchemicalbank/wizards/Gamba467951.java
@@ -8,7 +8,7 @@ public class Gamba467951 extends Wizard {
 	@Override
 	public Decision askKeepOrLiquidate(int fundValue, int timespan) {
 		// TODO Auto-generated method stub
-		if (fundValue>400) {
+		if (fundValue>200) {
 			return Decision.LIQUIDATE_FUND;
 		}
 		else {
