d2b18ede5201f311479b71a009e7cf8f99e14943
AmatoFrancesco99 <francesco.amato01@universitadipavia.it>
2021-03-16 18:05:57 +0100

    Aggiungo Wizard

--- src/it/unipv/ingsw/alchemicalbank/wizards/Amato468497_Wizard.java
+++ src/it/unipv/ingsw/alchemicalbank/wizards/Amato468497_Wizard.java
@@ -1,5 +1,5 @@
 /**
- * @author Amato Francesco 466558
+ * @author Amato Francesco 468497
  */
 
 package it.unipv.ingsw.alchemicalbank.wizards;
@@ -10,15 +10,46 @@ import it.unipv.ingsw.alchemicalbank.Wizard;
 
 public class Amato468497_Wizard extends Wizard{
 
+	//Informazioni del mio mago
+	private long myCoins; //le mie monete
+	private int  myRevenue; //il mio guadagno sulla singola giocata
+	private int order, time; //chi gioca per primo ed il tempo passato (mesi)
+	
+	//Informazioni del mago contro cui sto attualmente giocando contro
+	private long hisCoins; //monete del mago avversario
+	private int hisRevenue; //guadagno del mio avversario sulla giocata
+	
+	
+	//Metodo per decidere la strategia migliore di quando chiudere o tenere
+	//aperto il fondo, per poi riuscire a vincere l'intero "tournament"
 	@Override
-	public Decision askKeepOrLiquidate(int fundValue, int timespan) {
+    public Decision askKeepOrLiquidate(int fundValue, int timespan) {
 		
-		if(timespan >= 10){
+		if(this.order == 2 && timespan == 11)
 			return Decision.LIQUIDATE_FUND;
-		} else {
+		else if (this.order == 1 && timespan == 10)
+			return Decision.LIQUIDATE_FUND;
+		else if (this.order == 2 && myRevenue>hisRevenue)
+			return Decision.LIQUIDATE_FUND;
+		else
 			return Decision.KEEP_FUND;
-		}
 		
-	}
+    }
+	
+	/* Metodi per ricavare le informazioni sulle mosse e le informazioni
+	   degli altri giocatori, per poi scegliere la strategia migliore */
+	@Override
+	public void newFund(int year, int order, long yourCoins, long partnerCoins) {
+		this.myCoins = yourCoins;
+		this.hisCoins = partnerCoins;
+		this.order = order;
+    }
+	
+	@Override
+	public void fundClosed(int time, int yourRevenue, int partnerRevenue) {
+        this.myRevenue = yourRevenue;
+        this.hisRevenue = partnerRevenue;
+        this.time = time;
+    }
 
 }
