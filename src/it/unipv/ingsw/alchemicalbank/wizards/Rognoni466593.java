f3b607464d714cf496c6822dd45dde8bb202b45a
Filippo Rognoni <filipporognoni@iMac-di-Filippo.local>
2021-03-16 20:09:58 +0100

    Aggiunta modifiche

--- src/it/unipv/ingsw/alchemicalbank/wizards/Rognoni466593.java
+++ src/it/unipv/ingsw/alchemicalbank/wizards/Rognoni466593.java
@@ -8,7 +8,23 @@ public class Rognoni466593 extends Wizard {
 	@Override
 	public Decision askKeepOrLiquidate(int fundValue, int timespan) {
 		// TODO Auto-generated method stub
-		return Decision.KEEP_FUND;
+		if(timespan >= 10) {
+			return Decision.LIQUIDATE_FUND;
+		}else {
+			return Decision.KEEP_FUND;
+		}
+	}
+
+	@Override
+	public void newFund(int year, int order, long yourCoins, long partnerCoins) {
+		// TODO Auto-generated method stub
+		super.newFund(year, order, yourCoins, partnerCoins);
+	}
+
+	@Override
+	public void fundClosed(int time, int yourRevenue, int partnerRevenue) {
+		// TODO Auto-generated method stub
+		super.fundClosed(time, yourRevenue, partnerRevenue);
 	}
 
 }
