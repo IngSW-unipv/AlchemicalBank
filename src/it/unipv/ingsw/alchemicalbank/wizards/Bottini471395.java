40d82bef5aed76328a3003e678391089eca3c75e
mattiabottini <mattia.bottini01@universitadipavia.it>
2021-03-16 16:51:37 +0100

    Update Bottini471395.java

--- src/it/unipv/ingsw/alchemicalbank/wizards/Bottini471395.java
+++ src/it/unipv/ingsw/alchemicalbank/wizards/Bottini471395.java
@@ -4,7 +4,7 @@ import it.unipv.ingsw.alchemicalbank.*;
 public class Bottini471395 extends Wizard{
 	@Override
 	public Decision askKeepOrLiquidate(int fundValue, int timespan) {
-		if(fundValue>100) {
+		if(fundValue>4000) {
 			return Decision.LIQUIDATE_FUND;
 		} else {
 			return Decision.KEEP_FUND;
