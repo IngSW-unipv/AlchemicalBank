dacc0f7b2072f599b1ff6f0e09b2cd78328f3c4f
mirtexxan <31573172+mirtexxan@users.noreply.github.com>
2021-03-16 15:58:53 +0100

    Aggiornato il wizard Musci

---
+++ src/it/unipv/ingsw/alchemicalbank/wizards/Musci234567.java
@@ -0,0 +1,19 @@
+package it.unipv.ingsw.alchemicalbank.wizards;
+
+import it.unipv.ingsw.alchemicalbank.Decision;
+import it.unipv.ingsw.alchemicalbank.Wizard;
+
+// Mirto Musci 123456
+
+public class Musci234567 extends Wizard {
+
+	@Override
+	public Decision askKeepOrLiquidate(int fundValue, int timespan) {
+		if (fundValue > 200) {
+			return Decision.LIQUIDATE_FUND;
+		} else {
+			return Decision.KEEP_FUND;
+		}
+	}
+
+}
