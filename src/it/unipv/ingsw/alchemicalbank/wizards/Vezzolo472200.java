6bfb96b3c3a42fc277a4a74cbc7723b741e77d4f
GaiaVezzolo <gaia.vezzolo01@universitadipavia.it>
2021-03-16 17:36:30 +0100

    Aggiunta Wizard Vezzolo472200

---
+++ src/it/unipv/ingsw/alchemicalbank/wizards/Vezzolo472200.java
@@ -0,0 +1,20 @@
+package it.unipv.ingsw.alchemicalbank.wizards;
+
+import it.unipv.ingsw.alchemicalbank.Decision;
+import it.unipv.ingsw.alchemicalbank.Wizard;
+
+// Vezzolo Gaia 
+
+public class Vezzolo472200 extends Wizard {
+
+	@Override
+	public Decision askKeepOrLiquidate(int fundValue, int timespan) {
+		
+		if (timespan>10) {
+			return Decision.LIQUIDATE_FUND;
+		}
+		else {
+			return Decision.KEEP_FUND;
+		}
+	}
+}
