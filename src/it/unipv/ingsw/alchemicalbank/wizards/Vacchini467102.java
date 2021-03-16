2beb7139c66439873e584d476f15a13a380e7ef3
Edoardo Vacchini <edoardo.vacchini01@universitadipavia.it>
2021-03-16 19:32:37 +0100

    202103161932

--- src/it/unipv/ingsw/alchemicalbank/wizards/Vacchini467102.java
+++ src/it/unipv/ingsw/alchemicalbank/wizards/Vacchini467102.java
@@ -8,18 +8,9 @@ public class Vacchini467102 extends Wizard{
 	private long myCoins, hisCoins;
 	private int order, time, myRevenue, hisRevenue, year;
 	
-	private int avgClosingTimeOfPartner, timesClosedByPartner;
-	
 	@Override
-    public Decision askKeepOrLiquidate(int fundValue, int timespan) {
-		if(this.year == 1) {
-			this.avgClosingTimeOfPartner = 0;
-			this.timesClosedByPartner = 0;
-		}
-		
-		
-		
-		if(timespan == 10 && timespan == 11)
+    public Decision askKeepOrLiquidate(int fundValue, int timespan) {		
+		if(timespan == 10 && timespan == 11) //soluzione di miglior guadagno tenendo conto dei patient
 			return Decision.LIQUIDATE_FUND;
 		else
 			return Decision.KEEP_FUND;
@@ -38,13 +29,5 @@ public class Vacchini467102 extends Wizard{
         this.myRevenue = yourRevenue;
         this.hisRevenue = partnerRevenue;
         this.time = time;
-        // stimo il tempo medio di chiusura del partner (utile se questo ha una strategia basata
-        // sulla soglia o sul valore del fondo).
-        if(((time % 2 == 0) && (this.order == 1))||((this.order == 2)&&(time %2 == 1))) { 
-        	this.timesClosedByPartner++;
-        	this.avgClosingTimeOfPartner *= (this.timesClosedByPartner-1)/this.timesClosedByPartner;
-        	this.avgClosingTimeOfPartner += (time/this.timesClosedByPartner);
-        }else
-        	return;
     }
 }
