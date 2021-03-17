341bf0074520c669ad6a8178f9e91593ca9b34a5
CamillaCP <80338467+CamillaCP@users.noreply.github.com>
2021-03-17 14:37:31 +0100

    Update wizard CespiPolisiani

--- src/it/unipv/ingsw/alchemicalbank/wizards/CespiPolisiani468935.java
+++ src/it/unipv/ingsw/alchemicalbank/wizards/CespiPolisiani468935.java
@@ -6,8 +6,15 @@ import it.unipv.ingsw.alchemicalbank.Wizard;
 
 public class CespiPolisiani468935 extends Wizard {
 	
+	
+	public int getRandomTimespan(int minTimespan, int maxTimespan) {
+		return (int)((Math.random() * (maxTimespan - minTimespan)) + minTimespan);
+	}
+	
 	public Decision  askKeepOrLiquidate(int fundValue, int timespan) {
-		if (timespan == 11) {
+		int randomTimespan = getRandomTimespan(7,12);
+		
+		if (timespan == randomTimespan) {
 			return Decision.LIQUIDATE_FUND;
 		} else {
 			return Decision.KEEP_FUND;
