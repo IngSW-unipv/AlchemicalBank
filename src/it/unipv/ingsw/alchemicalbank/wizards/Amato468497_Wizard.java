9c2bb6c2959a881b80df61cda1d8ec5fc0a76035
AmatoFrancesco99 <francesco.amato01@universitadipavia.it>
2021-03-18 18:01:34 +0100

    AmatoFrancesco: Modified his wizard to improve 'win skills'

--- src/it/unipv/ingsw/alchemicalbank/wizards/Amato468497_Wizard.java
+++ src/it/unipv/ingsw/alchemicalbank/wizards/Amato468497_Wizard.java
@@ -10,23 +10,28 @@ import it.unipv.ingsw.alchemicalbank.Wizard;
 
 public class Amato468497_Wizard extends Wizard{
 
-	//Informazioni del mio mago
-	private long myCoins; //le mie monete
-	private int  myRevenue; //il mio guadagno sulla singola giocata
-	private int order, time; //chi gioca per primo ed il tempo passato (mesi)
-	
-	//Informazioni del mago contro cui sto attualmente giocando contro
-	private long hisCoins; //monete del mago avversario
-	private int hisRevenue; //guadagno del mio avversario sulla giocata
-	
+	// Indica quale tra i giocatori tocca decidere se chiudere il conto
+	private int order;
 	
 	//Metodo per decidere la strategia migliore di quando chiudere o tenere
 	//aperto il fondo, per poi riuscire a vincere l'intero "tournament"
 	@Override
     public Decision askKeepOrLiquidate(int fundValue, int timespan) {
-		//UPDATE IN CORSO
-		if (((fundValue)/2)>(0.8)*myCoins) return Decision.LIQUIDATE_FUND;
-		else if (timespan==11) return Decision.LIQUIDATE_FUND;
+		
+		/* Provo a massimizzare il mio profitto, in base a chi toccherà
+		* la scelta di chiudere il conto il prossimo mese */
+		
+		// Se mi trovo nel mese 11 e la prossima volta il giocatore 2 
+		// chiuderà il fondo (al 12 mese il fondo viene chiuso in base alle
+		// regole del gioco) allora chiudo io per primo
+		if ((order++==2) && (timespan==11))
+			return Decision.LIQUIDATE_FUND;
+		// Se mi trovo nel mese 10 e al prossimo turno toccherà decidere
+		// se chiudere all'altro giocatore, è abbastanza ragionevole che
+		// chiuda prima di lui, altrimenti si intasca più soldi lui
+		else if ((order++==2) && (timespan==10)) 
+			return Decision.LIQUIDATE_FUND;
+		// Viceversa continuo a tenere aperto il conto
 		else return Decision.KEEP_FUND;
     }
 	
@@ -34,16 +39,6 @@ public class Amato468497_Wizard extends Wizard{
 	   degli altri giocatori, per poi scegliere la strategia migliore */
 	@Override
 	public void newFund(int year, int order, long yourCoins, long partnerCoins) {
-		this.myCoins = yourCoins;
-		this.hisCoins = partnerCoins;
 		this.order = order;
     }
-	
-	@Override
-	public void fundClosed(int time, int yourRevenue, int partnerRevenue) {
-        this.myRevenue = yourRevenue;
-        this.hisRevenue = partnerRevenue;
-        this.time = time;
-    }
-
 }
