6d3228ac8e10fb131f67ba76d37a163e9e334e27
aiman-al-masoud <aiman.almasoud01@universitadipavia.it>
2021-03-19 11:18:24 +0100

    Now my wizard knows in advance who he's up against

--- src/it/unipv/ingsw/alchemicalbank/wizards/AimanAlMasoud472462.java
+++ src/it/unipv/ingsw/alchemicalbank/wizards/AimanAlMasoud472462.java
@@ -1,164 +1,265 @@
 package it.unipv.ingsw.alchemicalbank.wizards;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Field;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Random;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-
-
-
+import io.github.classgraph.ClassGraph;
+import io.github.classgraph.ClassInfoList;
+import io.github.classgraph.ScanResult;
+import it.unipv.ingsw.alchemicalbank.AlchemicalBank;
+import it.unipv.ingsw.alchemicalbank.Bank;
 import it.unipv.ingsw.alchemicalbank.Decision;
 import it.unipv.ingsw.alchemicalbank.Wizard;
 
 //Aiman Al Masoud 472462
-//This wizard takes a statistical approach on the matter of liquidating his fund or not.
+//the idea is to keep track of each participant's capital through the logs, 
+//and then try to identify the participant through their current capital, which is
+//provided by the startNewFund() method.
+
+//Problems with this approach:
+//some of the contendents aren't relying solely on the information provided by the arguments
+//of the askKeepOrLiquidate method. Some are using Randoms, and others are doing all sorts of things
+//with global variables in their classes.
+//Since I can't access the actual instance of their classes that is playing against me, 
+//I can't predict exactly what their Decisions will be.
+	
 
 public class AimanAlMasoud472462 extends Wizard {
 
+	//how many coins my current opponent has
+	long currentOpponentsCapital;
 	
-	//critical/threshold fund values of (most) of the other contendents.
-	//ie: the fund values at which each other wizard liquidates their funds.
-	ArrayList<Integer> fundValues; 
+	//transactions' logger: exploiting logs to get cumulative info on 
+	//opponents' current wealth
+	Logger transactionsLogger = Logger.getLogger(Bank.class.getName());
+	
+	//latest transaction 
+	String lastTransactionMessage;
+	
+	//list of wizards and their capitals 
+	HashMap<Wizard, Long> wizardsAndCapitals = getAllWizards();
 	
 	public AimanAlMasoud472462() {
-		//get all of the critical fund values
-		fundValues = getFundValues();
+		
+		
+		//DEBUG PRINTOUT
+		/*
+		for(Wizard wizard: wizardsAndCapitals.keySet()) {
+			System.out.println(wizard + " "+wizardsAndCapitals.get(wizard));
+		}
+		*/
+		
+		
+		try {
+			transactionsLogger.addHandler(new Handler() {
+				@Override
+				public void close() throws SecurityException {
+				}
+				@Override
+				public void flush() {	
+				}
+				@Override
+				public void publish(LogRecord arg0) {
+					
+					lastTransactionMessage = arg0.getMessage();	
+					
+					//update capitals of two opponents
+					try {
+						int startIndex = lastTransactionMessage.indexOf(" months: ")+" months: ".length();
+						lastTransactionMessage = lastTransactionMessage.substring(startIndex);
+						String[] twoParts = lastTransactionMessage.split("/ ");
+						
+						
+						//this is the pattern in each of the twoParts 
+						//NameOfWizard\\whitespace(ProfitOfWizard)
+						Pattern pattern = Pattern.compile("(.*?)\\s\\((.*?)\\)");
+						
+						
+						//wizard 1 => parts[0]
+						Matcher matcher = pattern.matcher(twoParts[0]);
+						matcher.find();
+						String wizardOneName = matcher.group(1).trim();
+						long wizardOneProfit = Long.parseLong((matcher.group(2).trim()));
+						
+						
+						//wizard 2 => parts[1]
+						matcher = pattern.matcher(twoParts[1]);
+						matcher.find();
+						String wizardTwoName = matcher.group(1).trim();
+						long wizardTwoProfit = Long.parseLong((matcher.group(2).trim()));
+						
+						
+						//update wizards
+						updateWizardsCapital(wizardOneName, wizardOneProfit);
+						updateWizardsCapital(wizardTwoName, wizardTwoProfit);
+						
+					}catch(StringIndexOutOfBoundsException | ArrayIndexOutOfBoundsException | IllegalStateException e) {	
+					}
+					
+				}
+				
+			});
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} 
 	}
+	
 	
 	@Override
 	public Decision askKeepOrLiquidate(int fundValue, int timespan) {
 		
-		//if getDistribution(fundValue) > 0.5, it's likely that 
-		//my opponent would liquidate if given a chance to, 
-		//so I'll liquidate before they can do it.
-		if(getDistribution(fundValue)>=0.5) {
-			return Decision.LIQUIDATE_FUND;
-		}else {
-			return Decision.KEEP_FUND;
-		}
-	}
-	
-	
-	//fundValue can be derived from timespan.
-	//get fundValue from time span
-	public int timespanToFundValue(int timespan) {
-		return (int)(20*Math.pow(2, timespan-1));
-	}
-	
-	//read threshhold fund values from other Wizards
-	public ArrayList<Integer> getFundValues(){
-		ArrayList<Integer> fundValues = new ArrayList<Integer>();
-		
-		//list all of the Wizards' source code files
-		for(File file : new File("./src/it/unipv/ingsw/alchemicalbank/wizards").listFiles()) {
-			try {
-				BufferedReader reader = new BufferedReader(new FileReader(file));
-				String lineBuffer = null;
-				while((lineBuffer = reader.readLine())!=null) {
-					if(lineBuffer.contains("fund")) {
-						int numericValue =parseString(lineBuffer);
-						if(numericValue>0 && numericValue<41000) {
-							fundValues.add(numericValue);
-						}
-					}else if(lineBuffer.contains("timespan")) {
-						int numericValue =parseString(lineBuffer);
-						if(numericValue>0 && numericValue<12) {
-							fundValues.add(timespanToFundValue(numericValue));
-						}
-					}
-				}
-				reader.close();
-			} catch (Exception e) {
-			}
-			
-		}
-		
-		return fundValues;
-	}
-	
-	
-	//parse string for a number
-	public int parseString(String line) {
-		String buffer = "";
-		for(int i = 0; i<line.length(); i++) {
-			if(Character.isDigit(line.charAt(i))) {
-				buffer+=line.charAt(i);
-			}
-		}
-		
 		try {
-			return Integer.parseInt(buffer);
-		}catch(NumberFormatException e) {
-			return -1;
+			//find who my current opponent is
+			Wizard currentOpponent = findWizardByCapital(currentOpponentsCapital);
+			
+			//DEBUG PRINTOUT
+			//System.out.println("my current opponent is: "+currentOpponent);
+			
+			
+			//find out weather they'd liquidate on their next turn:
+			if(currentOpponent.askKeepOrLiquidate(2*fundValue, timespan+1)==Decision.LIQUIDATE_FUND) {
+				return Decision.LIQUIDATE_FUND; //if they would, do it before they can.
+			}else if(timespan==11) {
+				return Decision.LIQUIDATE_FUND;
+			}else {
+				return Decision.KEEP_FUND; 
+			}
+			
+			
+		}catch(NullPointerException e) {
+			//if this fails
+			return Decision.LIQUIDATE_FUND;
+		}
+		
+	}
+
+	
+	//get instances of all wizard objects (except for this class)
+	public HashMap<Wizard, Long> getAllWizards(){
+		
+		long STARTING_BALANCE = 100;
+		HashMap<Wizard, Long> accountsBuffer = new HashMap<Wizard, Long>();
+		
+		// Add one instance of all descendants of the Wizard class
+        try (ScanResult scanResult = new ClassGraph().enableAllInfo().whitelistPackages("it.unipv.ingsw.alchemicalbank.wizards")
+                .scan()) {
+        	ClassInfoList controlClasses = scanResult.getSubclasses("it.unipv.ingsw.alchemicalbank.Wizard");
+        	List<Class<?>> controlClassRefs = controlClasses.loadClasses();
+        	for (Class<?> c : controlClassRefs) {
+        		try {
+        			if(!c.toString().contains("Aiman")) {
+        				Wizard new_client = (Wizard)c.getDeclaredConstructor().newInstance();
+    					accountsBuffer.put(new_client, STARTING_BALANCE);
+        			}
+        		} catch (Exception e) {
+				} 
+        	}
+        }
+
+        // Add one extra 'WackyWizard' if they are in an odd number
+        if (accountsBuffer.size() % 2 != 0)
+        	accountsBuffer.put(new WackyWizard(), STARTING_BALANCE);
+		
+        return accountsBuffer;
+	}
+        	
+
+
+	//exploit this method to get the current value of my oppponent's capital
+	@Override
+	public void newFund(int year, int order, long yourCoins, long partnerCoins) {
+		this.currentOpponentsCapital = partnerCoins;
+		super.newFund(year, order, yourCoins, partnerCoins);
+	}
+	
+	
+	//update a single wizard's capital
+	public void updateWizardsCapital(String wizardName, long profit) {
+		Wizard wizardGettingUpdated = findWizard(wizardName);
+		try {
+			wizardsAndCapitals.put(wizardGettingUpdated, wizardsAndCapitals.get(wizardGettingUpdated)+profit);
+		}catch(NullPointerException e) {
+			//wizard doesn't exist, or it's myself
 		}
 	}
 	
 	
-	//sort by fund value (which is the KEY of these Entries). In ascending order.
-	public ArrayList<Entry<Integer, Integer>> getSortedFreqList() {
+	//find wizard in map
+	public Wizard findWizard(String wizardName) {
+		Wizard foundWizard = null;
+		//find wizard by name inside of the map
 		
-		  HashMap<Integer, Integer> freqMap = new HashMap<Integer, Integer>();
-			//get all frequencies
-			for(Integer num : fundValues) {
-				if(freqMap.containsKey(num)) {
-					freqMap.put(num, freqMap.get(num)+1);
-				}else {
-					freqMap.put(num, 1);
-				}
+		for(Wizard wizard : wizardsAndCapitals.keySet()) {
+			if(wizard.toString().contains(wizardName)) {
+				foundWizard = wizard;
+				break;
 			}
-		   
-		//get sorted list of entries
-			ArrayList<Entry<Integer, Integer>> list = new ArrayList<>(freqMap.entrySet());
-			
-			Collections.sort(list, new Comparator<Entry>() {
-				@Override
-				public int compare(Entry arg0, Entry arg1) {
-					return (int)arg0.getKey() - (int)arg1.getKey();
-				}
-			});
-			
-		return list;
+		}
+		return foundWizard;
 	}
 	
-	
-	//F() distribution function. Input= fundValue, output= cumulative frequency
-	public Float getDistribution(int fundValue) {
-		ArrayList<Entry<Integer, Integer>> list = getSortedFreqList();
-		float accumul = 0;
+	//find wizard by current capital
+	//NB: if there's still two wizards with the same capital
+	//this method returns the first in the entry set.
+	public Wizard findWizardByCapital(long capital) {
 		
-		for(Entry entry : list) {
-			Integer value = (Integer)entry.getValue();
-			accumul += (float)value/fundValues.size();
-			
-			if(fundValue <= (Integer)entry.getKey()) {
-				return accumul;
+		for(Entry entry : wizardsAndCapitals.entrySet()) {
+			if( (long)entry.getValue() == capital) {
+				return (Wizard)entry.getKey();
 			}
 		}
 		
-		return accumul;
+		return null;
 	}
-	
-	
-	
-	/*
-	public static void main(String args[]) {
-		
-		AimanAlMasoud472462 wizard = new AimanAlMasoud472462();
-		
-		
-	}
-	*/
 	
 	
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
+	
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+
+	
+	
+
+
