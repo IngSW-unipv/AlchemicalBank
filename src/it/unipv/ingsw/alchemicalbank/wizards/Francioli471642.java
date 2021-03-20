92f171a0acb464fb403f0912b8c5f651e33e45bb
Lucia-7 <80346520+Lucia-7@users.noreply.github.com>
2021-03-20 12:10:23 +0100

    update strategia

--- src/it/unipv/ingsw/alchemicalbank/wizards/Francioli471642.java
+++ src/it/unipv/ingsw/alchemicalbank/wizards/Francioli471642.java
@@ -1,32 +1,46 @@
 package it.unipv.ingsw.alchemicalbank.wizards;
 
 import java.util.*;
+import java.lang.*;
 
 // @author Francioli Lucia 471642
 import it.unipv.ingsw.alchemicalbank.*;
 
 public class Francioli471642 extends Wizard {
-	//private ArrayList<Integer> close=new ArrayList<Integer>();
-	//private int sum,move;
+	private int sum,move,index,actualYear;
+	private ArrayList<Integer> close=new ArrayList<Integer>();
 	
 	
+	public void newFund(int year, int order, long yourCoins, long partnerCoins) {
+		this.actualYear = year;
+	}
 	
 	public Decision askKeepOrLiquidate(int fundValue, int timespan) {
-		//for(Integer x: close) {
-			//sum+=x;
-		//}
-		//move=(sum/close.size())-1;
-		if(timespan>=10)
+		if(actualYear<60) {
+			if(timespan>=10)
+			return Decision.LIQUIDATE_FUND;
+		else
+			return Decision.KEEP_FUND;
+		}else {
+			for(Integer x: close) {
+				sum+=(int)x;
+		}
+		move=(sum/close.size());
+		move-=1;
+		if(timespan==move)
 			return Decision.LIQUIDATE_FUND;
 		 else 
 			
 			return Decision.KEEP_FUND;
 						
 	}
+	}
 	
-	//public void fundClosed(int time, int yourRevenue, int partnerRevenue) {
-		//close.add(time);
-	//}
+	
+	public void fundClosed(int time, int yourRevenue, int partnerRevenue) {
+		close.add((Integer)time);
+		
+	}
 	
 	
 
