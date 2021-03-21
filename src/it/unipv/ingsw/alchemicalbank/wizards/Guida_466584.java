7e840d13a406024fd6327e31a1f263782e70806a
CaterinaGuida <80213159+CaterinaGuida@users.noreply.github.com>
2021-03-21 20:10:53 +0100

    aggiorno

--- src/it/unipv/ingsw/alchemicalbank/wizards/Guida_466584.java
+++ src/it/unipv/ingsw/alchemicalbank/wizards/Guida_466584.java
@@ -1,5 +1,58 @@
 package it.unipv.ingsw.alchemicalbank.wizards;
+import it.unipv.ingsw.alchemicalbank.Decision;
+import it.unipv.ingsw.alchemicalbank.Wizard;
 
-public class Guida_466584 {
+public class Guida_466584 extends Wizard {
+
+
+	private int order, year;
+	private long yourCoins, partnerCoins;
+	private boolean wonLastYear;
+	private int timeToClose;
+
+	
+
+	@Override
+	public Decision askKeepOrLiquidate(int fundValue, int timespan) {
+			if(timespan >= timeToClose) 
+				return Decision.LIQUIDATE_FUND;
+			 else 
+				return Decision.KEEP_FUND;
+	}
+
+	@Override
+	public void newFund(int year, int order, long yourCoins, long partnerCoins) {
+        this.year = year;
+		this.order = order;
+        this.yourCoins = yourCoins;
+        this.partnerCoins = partnerCoins;
+        timeToClose = timeToClose();
+    }
+
+	@Override
+	public void fundClosed(int time, int yourRevenue, int partnerRevenue) {
+        if(yourRevenue < partnerRevenue) {
+        	wonLastYear = false;
+        } else {
+        	wonLastYear = true;
+        }
+    }
+
+	private int timeToClose() {
+		int time = 11;
+		if(order == 2) {
+			time = 12;
+			if(!wonLastYear) {
+				time = time - 2;
+			}
+		}
+		if(year>=3 && yourCoins > 4*partnerCoins) {
+			if(order == 2)
+				time = 6;
+			else
+				time = 7;
+		}
+		return time;
+	}
 
 }
